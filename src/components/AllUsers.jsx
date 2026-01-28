b12bb53e2639c7b5ef4e6f4de9301c59a7c40bd1
sky-bala-20 <akashbala5002@gmail.com>
2026-01-28 18:10:03 +0530

    change

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 import '../style/AllUsers.css'
 
 const AllUsers = () => {
@@ -23,10 +28,20 @@ const AllUsers = () => {
       })
   }, [])
 
+  // Function to chunk users into groups of 5 for slides
+  const chunkUsers = (arr, size) => {
+    const chunks = []
+    for (let i = 0; i < arr.length; i += size) {
+      chunks.push(arr.slice(i, i + size))
+    }
+    return chunks
+  }
+
+  const userChunks = chunkUsers(users, 5)
+
   return (
     <div className="all-users-container">
-      <h1>All Candidate</h1>
-      {/* <Link to='/create-user'>Create User</Link> */}
+      <h1>All Candidates</h1>
       {loading && <p className="loading">loading...</p>}
       {error && <p className="error">{error}</p>}
       {!loading && !error && (
@@ -34,15 +49,28 @@ const AllUsers = () => {
           {users.length === 0 ? (
             <p className="no-users">No Users Available</p>
           ) : (
-            <ul className="user-list">
-              {users.map((user) => (
-                <li key={user._id}>
-                  <Link to={`/user-details/${user._id}`}>
-                    {user.name} - Click here to see
-                  </Link>
-                </li>
+            <Swiper
+              modules={[Navigation, Pagination]}
+              spaceBetween={20}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+              className="user-swiper"
+            >
+              {userChunks.map((chunk, index) => (
+                <SwiperSlide key={index} className="user-slide">
+                  <ul className="user-list">
+                    {chunk.map((user) => (
+                      <li key={user._id} className="user-item">
+                        <Link to={`/user-details/${user._id}`}>
+                          {user.name} - Click here to see
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </SwiperSlide>
               ))}
-            </ul>
+            </Swiper>
           )}
         </>
       )}
